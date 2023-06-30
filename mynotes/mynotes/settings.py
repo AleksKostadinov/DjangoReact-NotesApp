8aa713718ba47dc070931cb16e1156dbedb77c5a
AleksKostadinov <kostadinov12@gmail.com>
2023-06-30 23:05:03 +0300

    Installed django-cors-headers

--- mynotes/mynotes/settings.py
+++ mynotes/mynotes/settings.py
@@ -41,11 +41,13 @@ INSTALLED_APPS = [
     'api',
 
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
