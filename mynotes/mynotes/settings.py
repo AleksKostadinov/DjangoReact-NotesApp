eed7ffc464acd20e9069c125cee4b08a9e0b6d7a
AleksKostadinov <kostadinov12@gmail.com>
2023-07-02 19:57:41 +0300

    Deployment

--- mynotes/mynotes/settings.py
+++ mynotes/mynotes/settings.py
@@ -146,10 +146,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_RENDERER_CLASSES': [
-       'rest_framework.renderers.JSONRenderer',
-       'rest_framework.renderers.TemplateHTMLRenderer',
-    ],
+    # 'DEFAULT_RENDERER_CLASSES': [
+    #    'rest_framework.renderers.JSONRenderer',
+    #    'rest_framework.renderers.TemplateHTMLRenderer',
+    # ],
 }
 
 
