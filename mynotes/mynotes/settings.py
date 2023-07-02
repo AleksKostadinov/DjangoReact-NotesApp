0a0de69f4ecc3d26d55d867454b06b0c9cd83dc8
AleksKostadinov <kostadinov12@gmail.com>
2023-07-02 17:22:16 +0300

    Added Staticfiles

--- mynotes/mynotes/settings.py
+++ mynotes/mynotes/settings.py
@@ -128,9 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / 'frontend/build/static'),
+    BASE_DIR / 'frontend/build/static'
 ]
 
 # Default primary key field type
